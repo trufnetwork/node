4c179034c9f8d9e32a48d018a151582ea7a0a0bf
MicBun <michaelboentarman@gmail.com>
2024-02-17 20:28:12 +0700

    feat(home food): add extension meats_poultry_fish_eggs to read from included csv

--- truflation/extension.go
+++ truflation/extension.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kwilteam/kwil-db/extensions/actions"
 	"github.com/kwilteam/kwil-db/truflation/mathutil"
 	"github.com/kwilteam/kwil-db/truflation/tsn/basestream"
+	"github.com/kwilteam/kwil-db/truflation/tsn/meats_poultry_fish_eggs"
 	"github.com/kwilteam/kwil-db/truflation/tsn/stream"
 )
 
@@ -26,6 +27,11 @@ func init() {
 	}
 
 	err = actions.RegisterExtension("basestream", basestream.InitializeBasestream)
+	if err != nil {
+		panic(err)
+	}
+
+	err = actions.RegisterExtension("meats_poultry_fish_eggs", meats_poultry_fish_eggs.InitializeMeatsPoultryFishEggs)
 	if err != nil {
 		panic(err)
 	}
