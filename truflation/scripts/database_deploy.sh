34274d0f4a9db1f2c445b6e9021daff0c247b501
MicBun <michaelboentarman@gmail.com>
2024-02-27 09:00:03 +0700

    fix(food at home): Seperate deploy and composed script, since we need to replace DBID first

--- truflation/scripts/database_deploy.sh
+++ truflation/scripts/database_deploy.sh
@@ -9,9 +9,3 @@ sleep 5
 # deploy the databases
 kwil-cli database deploy -p=../food_and_beverages_schemas/foodstream.kf -n=meat
 kwil-cli database deploy -p=../food_and_beverages_schemas/foodstream.kf -n=cereal
-
-# wait for 5 seconds to make sure the databases are deployed on kwil node
-sleep 5
-
-# deploy the composed database
-kwil-cli database deploy -p=../food_and_beverages_schemas/composed_food_at_home.kf  -n=food_at_home
