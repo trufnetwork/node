c1b5f19468dd0bd076ea4990eab985f6c2fe192c
MicBun <michaelboentarman@gmail.com>
2024-02-29 22:47:40 +0700

    chore: separate batch add record into 2 part: cleaning and batch add script

--- truflation/scripts/batch_add_food_records.sh
+++ truflation/scripts/batch_add_food_records.sh
@@ -1,29 +1,3 @@
-# create temp_csv folder
-rm -rf ./temp_csv
-mkdir -p ./temp_csv
-
-# for each file on ./raw_csv, create temp file that have cleaned data
-# The header will be id, date_value, value for each file
-# for each column, multiply the value by 1000
-# save the file to ./temp_csv
-# seperate cpi, yahoo, nielsen, numbeo into different files
-# example meat_cpi.csv, meat_yahoo.csv, meat_nielsen.csv, meat_numbeo.csv
-for file in ./raw_csv/*.csv; do
-  echo "Processing $file"
-  if echo "$file" | grep -q "Away"; then
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $2*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_cpi.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $3*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_yahoo.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $4*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_numbeo.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $5*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_bigmac.csv
-  else
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $2*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_cpi.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $3*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_yahoo.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $4*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_nielsen.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $5*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_numbeo.csv
-  fi
-done
-
-# seed the database
 for file in ./temp_csv/*.csv; do
   echo "Processing file: $file"
   if echo "$file" | grep -q "Cereal" && echo "$file" | grep -q "cpi"; then
@@ -83,5 +57,3 @@ for file in ./temp_csv/*.csv; do
     echo "No match for $file"
   fi
 done
-
-rm -rf ./temp_csv
