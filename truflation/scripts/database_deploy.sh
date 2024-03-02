b381c1aea18cafde4e34a5bcd70764fde9b87247
Raffael Campos <raffael.rcampos@gmail.com>
2024-03-02 17:46:47 -0300

    Add count of schemas left to deploy in logs
    The commit integrates a feature to display the remaining numbers of primitive and composed schemas left to be deployed in the deployment logs. This update allows the database deploy script to provide more informative and helpful feedback during the schemas deployment process. Additionally, the .gitignore file was updated to exclude a new file "all_tables.csv".

--- truflation/scripts/database_deploy.sh
+++ truflation/scripts/database_deploy.sh
@@ -4,6 +4,8 @@ cd "$(dirname "$0")"
 
 echo "Deploying primitive schemas"
 
+primitive_count_left=$(ls ./temp_csv/*.csv | wc -l)
+
 # fore each csv file in temp_csv
 # drop the db, then run the deploy command
 for file in ./temp_csv/*.csv; do
@@ -13,12 +15,16 @@ for file in ./temp_csv/*.csv; do
     ../../.build/kwil-cli database drop "$filename" --sync
     echo "Deploying $filename"
     ../../.build/kwil-cli database deploy -p=../base_schema/base_schema.kf --sync --name="$filename" --sync
-    echo "Done"
+
+    $primitive_count_left=$((primitive_count_left-1))
+    echo "Done, $primitive_count_left to go"
 done
 
 echo "Done deploying primitive schemas"
 
 echo "Deploying composed schemas"
+
+$composed_count_left=$(ls ./temp_composed_schemas/*.json | wc -l)
 
 # for each file in temp_composed_schemas/*.json
 # drop the db, then run the deploy command
@@ -29,8 +35,8 @@ for file in ./temp_composed_schemas/*.json; do
     ../../.build/kwil-cli database drop "$filename" --sync
     echo "Deploying $filename"
     ../../.build/kwil-cli database deploy -p="$file" --type json --name "$filename" --sync
-    echo "Done"
-    echo "-"
+    $composed_count_left=$((composed_count_left-1))
+    echo "Done, $composed_count_left to go"
 done
 
 echo "All done"
