33492a96df26056015d3de60ff56e8484f274905
Raffael Campos <raffael.rcampos@gmail.com>
2024-03-04 05:01:02 -0300

    feat(composed indexes): Consolidate composed streams using a kuneiform generator (#58)
    * basic composed schema gen
    
    * Refactor composed schema deployment and generation scripts
    
    The deployment script has been moved to a new directory, "compose_scripts", and refactored to handle multiple schemas. Added two new bash scripts: "generate_composed_schemas.sh", which reads a CSV file and generates schema JSON for stream composition, and "composed_deploy.sh", which loop through those JSON files and deploys/drops databases accordingly. Additionally, created ".gitignore" file to exclude any temporary schema files.
    
    ---------
    
    Co-authored-by: Brennan Lamey <66885902+brennanjl@users.noreply.github.com>

---
+++ truflation/scripts/compose_scripts/generate_composed_schemas.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+# for each line ./composed_streams.csv
+# parse  columns being: 1: parent_stream, 2: stream, 3: weight with presence of the header
+
+# for each parent_stream, create a group of streams
+# then for each parent_stream group, run
+
+# run: go run ../cli.go -name <parent_stream> -import <1st stream>:<1st weight>,<2nd stream>:<2nd weight> -out temp_composed_schemas/<parent_stream>.json
+
+cd "$(dirname "$0")"
+
+declare -A stream_groups
+
+# Read the CSV file, skipping the header
+while IFS=, read -r parent_stream stream weight; do
+    # Append the stream and weight to the parent stream's entry in the associative array
+    stream_groups["$parent_stream"]+="/$stream:$weight,"
+done < <(tail -n +2 ../../composed_streams.csv)
+
+echo "Stream groups:"
+# Iterate over the associative array to print the stream groups
+for parent_stream in "${!stream_groups[@]}"; do
+    echo "$parent_stream: ${stream_groups[$parent_stream]}"
+done
+
+# Creates the directory if it doesn't exist
+mkdir -p ../temp_composed_schemas
+
+# Iterate over the associative array to run commands for each parent stream
+for parent_stream in "${!stream_groups[@]}"; do
+    # Remove the trailing comma from the list of streams and weights
+    streams_weights=${stream_groups[$parent_stream]%,}
+    # Run the command, substituting the placeholders with the actual values
+    go run ../../schema_gen/cli/cli.go -name "$parent_stream" -import "$streams_weights" -out "../temp_composed_schemas/$parent_stream.json"
+done
