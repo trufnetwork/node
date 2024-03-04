aabb7042f6e5447afac81109688968ade6b71903
Raffael Campos <raffael.rcampos@gmail.com>
2024-03-04 09:00:04 -0300

    Remove duplicated file deletion step in cleaning script

--- truflation/scripts/generate_clean_csv_from_raw.sh
+++ truflation/scripts/generate_clean_csv_from_raw.sh
@@ -13,10 +13,6 @@ mkdir -p ./temp_csv
 # for each column, multiply the value by 1000, make it an int
 # save the file to ./temp_csv
 for file in ./raw_from_db/*.csv; do
-  # if file exist, delete it
-  if [ -f ./temp_csv/$(basename $file) ]; then
-    rm ./temp_csv/$(basename $file)
-  fi
   echo "Processing $file"
   awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, int($2*1000)} {close("uuidgen")}' "$file" > ./temp_csv/$(basename $file)
 done
