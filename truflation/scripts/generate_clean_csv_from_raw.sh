565cdde519ab7ae94f3975fd502c86bd524cfa6d
Michael Buntarman <michaelboentarman@gmail.com>
2024-03-04 23:58:28 +0700

    chore: undo unnecessary changes

--- truflation/scripts/generate_clean_csv_from_raw.sh
+++ truflation/scripts/generate_clean_csv_from_raw.sh
@@ -1,6 +1,6 @@
 # create temp_csv folder
-rm -rf ./food_and_beverages_clean_csv
-mkdir -p ./food_and_beverages_clean_csv
+rm -rf ./temp_csv
+mkdir -p ./temp_csv
 
 # for each file on ./raw_csv, create temp file that have cleaned data
 # The header will be id, date_value, value for each file
@@ -11,14 +11,14 @@ mkdir -p ./food_and_beverages_clean_csv
 for file in ./food_and_beverages_raw_csv/*; do
   echo "Processing $file"
   if echo "$file" | grep -q "Away"; then
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $2*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_cpi.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $3*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_yahoo.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $4*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_numbeo.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $5*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_bigmac.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $2*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_cpi.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $3*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_yahoo.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $4*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_numbeo.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $5*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_bigmac.csv
   else
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $2*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_cpi.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $3*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_yahoo.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $4*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_nielsen.csv
-    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $5*1000} {close("uuidgen")}' "$file" > ./food_and_beverages_clean_csv/$(basename "$file" .csv)_numbeo.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $2*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_cpi.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $3*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_yahoo.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $4*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_nielsen.csv
+    awk -F, 'BEGIN {OFS=","} ("uuidgen" | getline uuid) > 0 {if (NR==1) print "id,date_value,value"; else print uuid, $1, $5*1000} {close("uuidgen")}' "$file" > ./temp_csv/$(basename "$file" .csv)_numbeo.csv
   fi
 done
