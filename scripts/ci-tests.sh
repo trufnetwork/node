04e193e90affc1d4f49511c10271e828edcfe610
Raffael Campos <raffael.rcampos@gmail.com>
2024-05-07 10:35:48 -0300

    Remove seed from CI and add basic ci test

---
+++ scripts/ci-tests.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+# file created to be used during CI tests actions. So the running node should be created from docker image
+
+# on error, exit
+set -e
+
+SUCCESS=0
+FAILURE=1
+
+function got_generic_error() {
+  echo "$1" | grep -i -E "error|fail|invalid" &> /dev/null
+  if [[ $? -eq 0 ]]; then
+    return $SUCCESS # found an error
+  fi
+
+  return $FAILURE
+}
+
+function expect_error() {
+  echo -e "Expecting error in output: \n$1\n"
+
+  if got_generic_error "$1"; then
+    echo -e "✅ Expected error found in output\n"
+    return $SUCCESS
+  fi
+
+  echo -e "❌ Expected error not found in output\n"
+  return $FAILURE
+}
+
+function expect_success() {
+  echo -e "Expecting success in output: \n$1\n"
+
+  if ! got_generic_error "$1"; then
+    echo -e "✅ Success found in output\n"
+    return $SUCCESS
+  fi
+
+  echo -e "❌ Success not found in output (error detected)\n"
+  return $FAILURE
+}
+
+echo -e "❓ Making sure we're able to ping the TSN node\n"
+expect_success "$(../.build/kwil-cli ping 2>&1)"
