1a8305086426a89eb4568d5406e38e0d6fffe398
Raffael Campos <raffael.rcampos@gmail.com>
2024-08-26 14:34:21 -0300

    Add integration test for Go binary execution
    Created a new Go script at `tests/hello/main.go` that prints "Hello, World!" and modified `asset_from_go_binary_test.go` to build and run this binary. This test now verifies the binary output to ensure it executes correctly from the expected S3 asset path.

--- deployments/infra/stacks/asset_from_go_binary_test.go
+++ deployments/infra/stacks/asset_from_go_binary_test.go
@@ -1,6 +1,9 @@
 package stacks
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
@@ -11,9 +14,26 @@ func TestBuildGoBinaryIntoS3Asset(t *testing.T) {
 	app := awscdk.NewApp(nil)
 	stack := awscdk.NewStack(app, jsii.String("test-stack"), nil)
 
-	buildGoBinaryIntoS3Asset(stack, jsii.String("test-asset"), buildGoBinaryIntoS3AssetInput{
-		BinaryPath: jsii.String("../../../cmd/benchmark/main.go"),
+	asset := buildGoBinaryIntoS3Asset(stack, jsii.String("test-asset"), buildGoBinaryIntoS3AssetInput{
+		BinaryPath: jsii.String("../tests/hello/main.go"),
+		BinaryName: jsii.String("hello"),
 	})
+
+	assetPath := asset.AssetPath()
+
+	fullPath := fmt.Sprintf("%s/%s/%s", *app.Outdir(), *assetPath, "hello")
+
+	// run and expect
+	os.Chmod(fullPath, 0755)
+	cmd := exec.Command(fullPath)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run binary: %s", err)
+	}
+
+	if string(output) != "Hello, World!\n" {
+		t.Fatalf("Expected 'Hello, World!', got %s", output)
+	}
 
 	app.Synth(nil)
 }
