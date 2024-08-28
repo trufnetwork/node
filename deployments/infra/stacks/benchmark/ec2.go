358619421745858bcf95155429d691e757018e51
Raffael Campos <raffael.rcampos@gmail.com>
2024-08-28 13:21:27 -0300

    Add error handling for nil values in EC2 launch template
    Ensure that critical properties such as launchTemplate, instanceType, userData, and S3 bucket name and object key are checked for nil values. This prevents potential runtime errors by panicking if these essential properties are not properly initialized.

--- deployments/infra/stacks/benchmark/ec2.go
+++ deployments/infra/stacks/benchmark/ec2.go
@@ -2,6 +2,7 @@ package benchmark
 
 import (
 	"fmt"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3assets"
@@ -29,15 +30,33 @@ type (
 func createLaunchTemplate(scope constructs.Construct, input CreateLaunchTemplateInput) CreateLaunchTemplateOutput {
 	// Create a new EC2 launch template with specified properties
 	launchTemplate := awsec2.NewLaunchTemplate(scope, jsii.String(input.ID), &awsec2.LaunchTemplateProps{
-		InstanceType:  input.InstanceType,
+		InstanceType: input.InstanceType,
+		// we want to use latest amazon linux
+		MachineImage:  awsec2.MachineImage_LatestAmazonLinux2(nil),
 		SecurityGroup: input.SecurityGroup,
 		KeyPair:       input.KeyPair,
+		UserData:      awsec2.UserData_ForLinux(nil),
 	})
+
+	// Check if launchTemplate is nil
+	if launchTemplate == nil {
+		panic("Failed to create launch template")
+	}
 
 	instanceType := input.InstanceType.ToString()
 
-	// Add user data to set and persist the instance type
-	launchTemplate.UserData().AddCommands(
+	// Check if instanceType is empty
+	if instanceType == nil || *instanceType == "" {
+		panic("Instance type is empty")
+	}
+
+	// Add user data
+	userData := launchTemplate.UserData()
+	if userData == nil {
+		panic("UserData is nil")
+	}
+
+	userData.AddCommands(
 		*jsii.Strings(
 			fmt.Sprintf("INSTANCE_TYPE=%s", *instanceType),
 			"echo INSTANCE_TYPE=$INSTANCE_TYPE >> /etc/environment",
@@ -46,8 +65,13 @@ func createLaunchTemplate(scope constructs.Construct, input CreateLaunchTemplate
 
 	benchmarkBinaryPath := "/home/ec2-user/benchmark"
 
+	// Check if S3 bucket name and object key are not nil
+	if input.BinaryS3Asset.S3BucketName() == nil || input.BinaryS3Asset.S3ObjectKey() == nil {
+		panic("S3 bucket name or object key is nil")
+	}
+
 	// Add user data to download and set up the benchmark binary
-	launchTemplate.UserData().AddCommands(
+	userData.AddCommands(
 		*jsii.Strings(
 			fmt.Sprintf("aws s3 cp s3://%s/%s %s",
 				*input.BinaryS3Asset.S3BucketName(),
