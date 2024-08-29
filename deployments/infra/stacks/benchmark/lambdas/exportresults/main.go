14afffffe887d4cfe300df442c45198404de8e16
Raffael Campos <raffael.rcampos@gmail.com>
2024-08-29 10:09:23 -0300

    Refactor exportresults function for improved CSV handling
    This commit refactors the export function to better handle CSV files by sorting them, appropriately parsing timestamps, and converting results to Markdown format. It upgrades the logic to efficiently download, process, and merge benchmark results before exporting them to S3. This results in a more robust and maintainable approach to result processing and storage.

--- deployments/infra/stacks/benchmark/lambdas/exportresults/main.go
+++ deployments/infra/stacks/benchmark/lambdas/exportresults/main.go
@@ -1,16 +1,19 @@
-package exportresults
+package main
 
 import (
 	"context"
 	"fmt"
-	"io"
+	"os"
 	"regexp"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/truflation/tsn-db/internal/benchmark/benchexport"
 )
 
 // Parameters:
@@ -19,12 +22,14 @@ import (
 
 type Event struct {
 	Bucket string `json:"bucket"`
-	Key    string `json:"key"`
+	// key = <timestamp>_<instance_type>.csv
+	Key string `json:"key"`
 }
 
 func HandleRequest(ctx context.Context, event Event) error {
 	// get the results from the results bucket
-	// all results are like this: s3://<bucket>/<key>_<instance_type>.csv
+	// all results are like this: s3://<bucket>/<timestamp>_<instance_type>.csv
+	// <timestamp> is the report time in 2024-05-02T12:00:00.000Z
 	// we want to get all the keys and then download them all into a tmp folder
 	// then we merge them into a single file
 
@@ -34,49 +39,73 @@ func HandleRequest(ctx context.Context, event Event) error {
 		Prefix: aws.String(event.Key),
 	})
 
-	header := ""
-	mergedContent := ""
+	csvFiles := make([]string, 0)
 
 	// get only csv files
 	for _, obj := range resp.Contents {
 		// matches /<key>_<instance_type>.csv
 		if regexp.MustCompile(`^.*_.*\.csv$`).MatchString(*obj.Key) {
-			// download the file
-			resp, err := s3Client.GetObject(&s3.GetObjectInput{
-				Bucket: aws.String(event.Bucket),
-				Key:    aws.String(*obj.Key),
-			})
-			if err != nil {
-				return err
-			}
-
-			content, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return err
-			}
-			lines := strings.Split(string(content), "\n")
-
-			// get header from first file
-			if header == "" {
-				header = lines[0]
-			}
-
-			mergedContent += strings.Join(lines[1:], "\n") + "\n"
+			csvFiles = append(csvFiles, *obj.Key)
 		}
+	}
+
+	// sort csv files
+	sort.Strings(csvFiles)
+
+	// download all the csv files
+	for _, csvFile := range csvFiles {
+		reportTime, err := time.Parse("2006-01-02T15:04:05.000Z", csvFile[:19])
+		// get instance type from the key
+		instanceType := csvFile[strings.LastIndex(csvFile, "_")+1:]
+
+		if err != nil {
+			return err
+		}
+
+		// download the file
+		resp, err := s3Client.GetObject(&s3.GetObjectInput{
+			Bucket: aws.String(event.Bucket),
+			Key:    aws.String(csvFile),
+		})
+
+		if err != nil {
+			return err
+		}
+
+		// Load the CSV file
+		results, err := benchexport.LoadCSV[benchexport.SavedResults](resp.Body)
+		if err != nil {
+			return err
+		}
+
+		// save the results to the merged file
+		benchexport.SaveAsMarkdown(benchexport.SaveAsMarkdownInput{
+			Results:      results,
+			CurrentDate:  reportTime,
+			InstanceType: instanceType,
+			FilePath:     "/tmp/results.md",
+		})
 	}
 
 	if err != nil {
 		return err
 	}
 
+	// upload the merged file to the results bucket
+
+	mergedFile, err := os.Open("/tmp/results.md")
+	if err != nil {
+		return err
+	}
+	defer mergedFile.Close()
+
 	fmt.Printf("Exporting results to s3://%s/%s.csv\n", event.Bucket, event.Key)
-	// TODO: export as markdown table
 
 	// upload the merged file to the results bucket
 	_, err = s3Client.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(event.Bucket),
 		Key:    aws.String(event.Key + ".csv"),
-		Body:   strings.NewReader(mergedContent),
+		Body:   mergedFile,
 	})
 
 	return err
