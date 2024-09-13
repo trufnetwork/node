1249fdaee81770aee50c7f3d159b5babf0c924ac
Raffael Campos <raffael.rcampos@gmail.com>
2024-09-13 09:17:30 -0300

    Add tests for composed data scenarios
    Introduce tests to validate composed data handling, covering scenarios with existing and no past data. This ensures robustness and correctness in data aggregation and retrieval functionalities.

---
+++ internal/contracts/tests/composed_test.go
@@ -0,0 +1,130 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/truflation/tsn-db/internal/contracts/tests/utils"
+	"github.com/truflation/tsn-sdk/core/util"
+
+	"github.com/kwilteam/kwil-db/core/utils"
+	kwilTesting "github.com/kwilteam/kwil-db/testing"
+	"github.com/pkg/errors"
+)
+
+func TestComposed(t *testing.T) {
+	kwilTesting.RunSchemaTest(t, kwilTesting.SchemaTest{
+		Name: "composed_test",
+		FunctionTests: []kwilTesting.TestFunc{
+			WithComposedTestSetup(testComposedLastAvailable(t)),
+			WithComposedTestSetup(testComposedNoPastData(t)),
+		},
+	})
+}
+
+func WithComposedTestSetup(testFn func(ctx context.Context, platform *kwilTesting.Platform) error) func(ctx context.Context, platform *kwilTesting.Platform) error {
+	return func(ctx context.Context, platform *kwilTesting.Platform) error {
+		// we just need to define a valid address here, as we don't need to deploy anything
+		deployerAddress := util.Unsafe_NewEthereumAddressFromString("0x0000000000000000000000000000000000000123")
+		platform.Deployer = deployerAddress.Bytes()
+
+		// Run the actual test function
+		return testFn(ctx, platform)
+	}
+}
+
+func testComposedLastAvailable(t *testing.T) func(ctx context.Context, platform *kwilTesting.Platform) error {
+	return func(ctx context.Context, platform *kwilTesting.Platform) error {
+		composedDBID := utils.GenerateDBID(composedStreamId.String(), platform.Deployer)
+
+		// Setup data for the test
+		err := testutils.SetupComposedFromMarkdown(ctx, testutils.MarkdownComposedSetupInput{
+			Platform:           platform,
+			ComposedStreamName: composedStreamName,
+			Height:             1,
+			MarkdownData: `
+				| date       | Stream 1 | Stream 2 | Stream 3 |
+				| ---------- | --------- | --------- | --------- |
+				| 2024-08-29 | 1         |           | 4         |
+				| 2024-08-30 |           |           |           |
+				| 2024-08-31 |           | 2         | 5         |
+				| 2024-09-01 |           |           | 3         |
+			`,
+			Weights: []string{"1", "2", "3"},
+		})
+		if err != nil {
+			return errors.Wrap(err, "error setting up last available test data")
+		}
+
+		result, err := testutils.GetRecord(ctx, testutils.GetRecordOrIndexInput{
+			Platform: platform,
+			DBID:     composedDBID,
+			DateFrom: "2024-08-29",
+			DateTo:   "2024-09-01",
+			Height:   0,
+		})
+		if err != nil {
+			return errors.Wrap(err, "error in testComplexComposedLastAvailable")
+		}
+
+		expected := `
+		| date       | value  |
+		| ---------- | ------ |
+		| 2024-08-29 | 3.250  | # 1 & 4
+		| 2024-08-30 | 		  |
+		| 2024-08-31 | 3.333  | # 1 & 2 & 5
+		| 2024-09-01 | 2.333  | # 1 & 2 & 3
+		`
+
+		testutils.AssertResultRowsEqualMarkdownTable(t, result, expected)
+
+		return nil
+	}
+}
+
+func testComposedNoPastData(t *testing.T) func(ctx context.Context, platform *kwilTesting.Platform) error {
+	return func(ctx context.Context, platform *kwilTesting.Platform) error {
+		composedDBID := utils.GenerateDBID(composedStreamId.String(), platform.Deployer)
+
+		// Setup data for the test
+		err := testutils.SetupComposedFromMarkdown(ctx, testutils.MarkdownComposedSetupInput{
+			Platform:           platform,
+			ComposedStreamName: composedStreamName,
+			Height:             1,
+			MarkdownData: `
+				| date       | Stream 1 | Stream 2 | Stream 3 |
+				| ---------- | --------- | --------- | --------- |
+				| 2024-08-30 | 1         |           |           |
+				| 2024-08-31 |           | 2         |           |
+				| 2024-09-01 |           |           | 3         |
+			`,
+			Weights: []string{"1", "2", "3"},
+		})
+		if err != nil {
+			return errors.Wrap(err, "error setting up no past data test")
+		}
+
+		result, err := testutils.GetRecord(ctx, testutils.GetRecordOrIndexInput{
+			Platform: platform,
+			DBID:     composedDBID,
+			DateFrom: "2024-08-30",
+			DateTo:   "2024-09-01",
+			Height:   0,
+		})
+		if err != nil {
+			return errors.Wrap(err, "error in testComplexComposedNoPastData")
+		}
+
+		expected := `
+		| date       | value  |
+		| ---------- | ------ |
+		| 2024-08-30 | 1.000  | # 1
+		| 2024-08-31 | 1.667  | # 1 & 2
+		| 2024-09-01 | 2.333  | # 1 & 2 & 3
+		`
+
+		testutils.AssertResultRowsEqualMarkdownTable(t, result, expected)
+
+		return nil
+	}
+}
