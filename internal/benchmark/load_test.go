9d166f632ebf924e89deae6838438ca652cba1f3
MicBun <michaelboentarman@gmail.com>
2024-10-21 15:49:00 +0700

    test: get monetary value with dynamic weight

--- internal/benchmark/load_test.go
+++ internal/benchmark/load_test.go
@@ -3,7 +3,6 @@ package benchmark
 import (
 	"context"
 	"fmt"
-	"math"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -69,42 +68,45 @@ func TestBench(t *testing.T) {
 	shapePairs := [][]int{
 		// qtyStreams, branchingFactor
 		// testing 1 stream only
-		{1, 1},
+		//{1, 1},
 
 		//flat trees = cost of adding a new stream to our composed
-		{50, math.MaxInt},
-		{100, math.MaxInt},
-		{200, math.MaxInt},
-		{400, math.MaxInt},
+		//{50, math.MaxInt},
+		//{100, math.MaxInt},
+		//{200, math.MaxInt},
+		//{400, math.MaxInt},
 		// 800 streams kills t3.small instances for memory starvation. But probably because it stores the whole tree in memory
 		//{800, math.MaxInt},
 		//{1500, math.MaxInt}, // this gives error: Out of shared memory
 
 		// deep trees = cost of adding depth
-		{50, 1},
-		{100, 1},
+		//{50, 1},
+		//{100, 1},
 		//{200, 1}, // we can't go deeper than 180, for call stack size issues
 
 		// to get difference for stream qty on a real world situation
-		{50, 8},
-		{100, 8},
+		//{50, 8},
+		//{100, 8},
 		{200, 8},
-		{400, 8},
+		//{400, 8},
 		//{800, 8},
 
 		// to get difference for branching factor
-		{200, 2},
-		{200, 4},
+		//{200, 2},
+		//{200, 4},
 		// {200, 8}, // already tested above
-		{200, 16},
-		{200, 32},
+		//{200, 16},
+		//{200, 32},
 	}
 
 	samples := 3
 
 	days := []int{1, 7, 30, 365}
 
-	visibilities := []util.VisibilityEnum{util.PublicVisibility, util.PrivateVisibility}
+	visibilities := []util.VisibilityEnum{
+		//util.PublicVisibility,
+		util.PrivateVisibility,
+	}
 
 	var functionTests []kwilTesting.TestFunc
 	// a channel to receive results from the tests
@@ -121,9 +123,9 @@ func TestBench(t *testing.T) {
 				Days:            days,
 				Procedures: []ProcedureEnum{
 					ProcedureGetRecord,
-					ProcedureGetIndex,
-					ProcedureGetChangeIndex,
-					ProcedureGetFirstRecord,
+					//ProcedureGetIndex,
+					//ProcedureGetChangeIndex,
+					//ProcedureGetFirstRecord,
 				},
 			},
 				// use pointer, so we can reassign the results channel
