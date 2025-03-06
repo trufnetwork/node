c750733449514120621e33a508306f4e661f1597
Raffael Campos <raffael.rcampos@gmail.com>
2025-03-06 16:44:54 -0300

    refactor: Update test utilities for streams with enhanced type safety and simplified interfaces
    This commit refactors the test utilities for streams, focusing on:
    - Replacing date-based inputs with event time integers
    - Simplifying markdown parsing and data insertion logic
    - Updating function signatures to use more precise types
    - Removing commented-out legacy code
    - Improving type conversion and error handling in test setup functions

--- tests/streams/utils/setup/common.go
+++ tests/streams/utils/setup/common.go
@@ -1,92 +1,47 @@
 package setup
 
-// import (
-// 	"context"
+import (
+	"context"
 
-// 	"github.com/pkg/errors"
+	"github.com/kwilteam/kwil-db/common"
+	kwilTesting "github.com/kwilteam/kwil-db/testing"
+	"github.com/trufnetwork/sdk-go/core/types"
+)
 
-// 	"github.com/kwilteam/kwil-db/common"
-// 	"github.com/kwilteam/kwil-db/core/utils"
-// 	"github.com/kwilteam/kwil-db/parse"
-// 	kwilTesting "github.com/kwilteam/kwil-db/testing"
-// 	"github.com/trufnetwork/sdk-go/core/util"
-// )
+type ContractType string
 
-// type ContractType string
+const (
+	ContractTypePrimitive ContractType = "primitive"
+	ContractTypeComposed  ContractType = "composed"
+)
 
-// const (
-// 	ContractTypePrimitive ContractType = "primitive"
-// 	ContractTypeComposed  ContractType = "composed"
-// )
+type StreamInfo struct {
+	Locator types.StreamLocator
+	Type    ContractType
+}
 
-// type ContractInfo struct {
-// 	Name     string
-// 	StreamID util.StreamId
-// 	Deployer util.EthereumAddress
-// 	Content  []byte
-// 	Type     ContractType
-// }
+// CreateStream parses and creates the dataset for a contract
+func CreateStream(ctx context.Context, platform *kwilTesting.Platform, contractInfo StreamInfo) (*common.CallResult, error) {
 
-// type InitializeContractInput struct {
-// 	Platform *kwilTesting.Platform
-// 	Deployer util.EthereumAddress
-// 	Dbid     string
-// 	Height   int64
-// }
+	txContext := &common.TxContext{
+		Ctx:          ctx,
+		BlockContext: &common.BlockContext{Height: 0},
+		Signer:       contractInfo.Locator.DataProvider.Bytes(),
+		Caller:       contractInfo.Locator.DataProvider.Address(),
+		TxID:         platform.Txid(),
+	}
 
-// func initializeContract(ctx context.Context, input InitializeContractInput) error {
-// 	txContext := &common.TxContext{
-// 		Ctx: ctx,
-// 		BlockContext: &common.BlockContext{
-// 			Height: input.Height,
-// 		},
-// 		TxID:   input.Platform.Txid(),
-// 		Signer: input.Deployer.Bytes(),
-// 		Caller: input.Deployer.Address(),
-// 	}
+	engineContext := &common.EngineContext{
+		TxContext: txContext,
+	}
 
-// 	_, err := input.Platform.Engine.Procedure(txContext, input.Platform.DB, &common.ExecutionData{
-// 		Procedure: "init",
-// 		Dataset:   input.Dbid,
-// 		Args:      []any{},
-// 	})
-// 	return err
-// }
-
-// // SetupAndInitializeContract sets up and initializes a contract for testing.
-// func SetupAndInitializeContract(ctx context.Context, platform *kwilTesting.Platform, contractInfo ContractInfo) error {
-// 	if err := setupContract(ctx, platform, contractInfo); err != nil {
-// 		return err
-// 	}
-// 	dbid := GetDBID(contractInfo)
-// 	return initializeContract(ctx, InitializeContractInput{
-// 		Platform: platform,
-// 		Deployer: contractInfo.Deployer,
-// 		Dbid:     dbid,
-// 		Height:   0,
-// 	})
-// }
-
-// // setupContract parses and creates the dataset for a contract
-// func setupContract(ctx context.Context, platform *kwilTesting.Platform, contractInfo ContractInfo) error {
-// 	schema, err := parse.Parse(contractInfo.Content)
-// 	if err != nil {
-// 		return errors.Wrapf(err, "Failed to parse contract %s", contractInfo.Name)
-// 	}
-// 	schema.Name = contractInfo.StreamID.String()
-
-// 	txContext := &common.TxContext{
-// 		Ctx:          ctx,
-// 		BlockContext: &common.BlockContext{Height: 0},
-// 		Signer:       contractInfo.Deployer.Bytes(),
-// 		Caller:       contractInfo.Deployer.Address(),
-// 		TxID:         platform.Txid(),
-// 	}
-
-// 	return platform.Engine.CreateDataset(txContext, platform.DB, schema)
-// }
-
-// // GetDBID generates a DBID from contract info
-// func GetDBID(contractInfo ContractInfo) string {
-// 	return utils.GenerateDBID(contractInfo.StreamID.String(), contractInfo.Deployer.Bytes())
-// }
+	return platform.Engine.Call(engineContext,
+		platform.DB,
+		"",
+		"create_stream",
+		[]any{contractInfo.Locator.StreamId.String(), contractInfo.Type},
+		func(row *common.Row) error {
+			return nil
+		},
+	)
+}
