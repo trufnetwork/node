7db2a51da2a1d5aa5224756262e1944172bec1ec
williamrusdyputra <williamrusdyputra@gmail.com>
2025-07-09 13:10:11 +0700

    chore: normalize metadata table

--- internal/migrations/017-normalize-tables.sql
+++ internal/migrations/017-normalize-tables.sql
@@ -13,3 +13,14 @@ FOREIGN KEY (data_provider_id) REFERENCES data_providers(id) ON DELETE CASCADE;
 
 CREATE UNIQUE INDEX IF NOT EXISTS streams_provider_stream_idx ON streams(data_provider_id, stream_id);
 CREATE UNIQUE INDEX IF NOT EXISTS streams_id_idx ON streams(id);
+
+/*----------------------------------------------------------------------
+ * Metadata
+ *---------------------------------------------------------------------*/
+
+ALTER TABLE metadata
+ADD COLUMN stream_ref UUID;
+
+CREATE INDEX IF NOT EXISTS meta_stream_to_key_idx ON metadata(stream_ref, metadata_key, created_at);
+CREATE INDEX IF NOT EXISTS meta_stream_to_ref_idx ON metadata(stream_ref, metadata_key, value_ref);
+CREATE INDEX IF NOT EXISTS meta_key_ref_to_stream_idx ON metadata(metadata_key, value_ref, stream_ref);
