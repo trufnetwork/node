a699da987958ee2b1fb2962061befef04ac55b7d
William <williamrusdyputra@gmail.com>
2025-09-09 11:33:23 +0700

    feat: lock and unlock TRUF tokens (#1145)
    * feat: lock and unlock TRUF tokens
    
    * chore: update escrow contract
    
    * chore: use text then convert to numeric on function

--- internal/migrations/erc20-bridge/001-actions.sql
+++ internal/migrations/erc20-bridge/001-actions.sql
@@ -4,8 +4,24 @@ CREATE OR REPLACE ACTION sepolia_wallet_balance($wallet_address TEXT) PUBLIC VIE
   return $balance;
 };
 
+CREATE OR REPLACE ACTION sepolia_admin_lock_tokens($wallet_address TEXT, $amount TEXT) PUBLIC {
+  sepolia_bridge.lock_admin($wallet_address, $amount::NUMERIC(78, 0));
+};
+
+CREATE OR REPLACE ACTION sepolia_admin_unlock_tokens($to_address TEXT, $amount TEXT) PUBLIC {
+  sepolia_bridge.unlock($to_address, $amount::NUMERIC(78, 0));
+};
+
 -- MAINNET
 CREATE OR REPLACE ACTION mainnet_wallet_balance($wallet_address TEXT) PUBLIC VIEW RETURNS (balance NUMERIC(78, 0)) {
   $balance := mainnet_bridge.balance($wallet_address);
   return $balance;
+};
+
+CREATE OR REPLACE ACTION mainnet_admin_lock_tokens($wallet_address TEXT, $amount TEXT) PUBLIC {
+  mainnet_bridge.lock_admin($wallet_address, $amount::NUMERIC(78, 0));
+};
+
+CREATE OR REPLACE ACTION mainnet_admin_unlock_tokens($to_address TEXT, $amount TEXT) PUBLIC {
+  mainnet_bridge.unlock($to_address, $amount::NUMERIC(78, 0));
 };
