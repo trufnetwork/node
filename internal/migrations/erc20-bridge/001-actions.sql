4ee7d51f293bed1e9992e8fe2a35a7414e3862be
williamrusdyputra <williamrusdyputra@gmail.com>
2025-09-15 11:29:29 +0700

    feat: add erc20 bridge info action

--- internal/migrations/erc20-bridge/001-actions.sql
+++ internal/migrations/erc20-bridge/001-actions.sql
@@ -1,7 +1,24 @@
+CREATE OR REPLACE ACTION get_erc20_bridge_info() 
+PUBLIC VIEW RETURNS (
+  chain TEXT,
+  escrow TEXT,
+  epoch_period TEXT,
+  erc20 TEXT,
+  decimals INT,
+  balance NUMERIC(78, 0),
+  synced BOOLEAN,
+  synced_at INT8,
+  enabled BOOLEAN
+) {
+  FOR $row IN sepolia_bridge.info() {
+    RETURN $row.chain, $row.escrow, $row.epoch_period, $row.erc20, $row.decimals, $row.balance, $row.synced, $row.synced_at, $row.enabled;
+  }
+};
+
 -- TESTNET
 CREATE OR REPLACE ACTION sepolia_wallet_balance($wallet_address TEXT) PUBLIC VIEW RETURNS (balance NUMERIC(78, 0)) {
   $balance := sepolia_bridge.balance($wallet_address);
-  return $balance;
+  RETURN $balance;
 };
 
 CREATE OR REPLACE ACTION sepolia_admin_bridge_tokens($amount TEXT) PUBLIC {
@@ -17,7 +34,7 @@ CREATE OR REPLACE ACTION sepolia_admin_bridge_tokens($amount TEXT) PUBLIC {
 -- MAINNET
 CREATE OR REPLACE ACTION mainnet_wallet_balance($wallet_address TEXT) PUBLIC VIEW RETURNS (balance NUMERIC(78, 0)) {
   $balance := mainnet_bridge.balance($wallet_address);
-  return $balance;
+  RETURN $balance;
 };
 
 CREATE OR REPLACE ACTION mainnet_admin_bridge_tokens($amount TEXT) PUBLIC {
