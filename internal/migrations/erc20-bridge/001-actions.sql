b8c8f98f774ed009b334e3b9cbc91f1c7dbfde92
williamrusdyputra <williamrusdyputra@gmail.com>
2025-10-23 10:51:10 +0700

    chore: update erc20 extension queries

--- internal/migrations/erc20-bridge/001-actions.sql
+++ internal/migrations/erc20-bridge/001-actions.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE ACTION get_erc20_bridge_info() 
+-- TESTNET
+CREATE OR REPLACE ACTION sepolia_get_erc20_bridge_info() 
 PUBLIC VIEW RETURNS (
   chain TEXT,
   escrow TEXT,
@@ -15,7 +16,6 @@ PUBLIC VIEW RETURNS (
   }
 };
 
--- TESTNET
 CREATE OR REPLACE ACTION sepolia_wallet_balance($wallet_address TEXT) PUBLIC VIEW RETURNS (balance NUMERIC(78, 0)) {
   $balance := sepolia_bridge.balance($wallet_address);
   RETURN $balance;
@@ -26,11 +26,28 @@ CREATE OR REPLACE ACTION sepolia_bridge_tokens($recipient TEXT DEFAULT NULL, $am
 };
 
 -- MAINNET
+CREATE OR REPLACE ACTION ethereum_get_erc20_bridge_info() 
+PUBLIC VIEW RETURNS (
+  chain TEXT,
+  escrow TEXT,
+  epoch_period TEXT,
+  erc20 TEXT,
+  decimals INT,
+  balance NUMERIC(78, 0),
+  synced BOOLEAN,
+  synced_at INT8,
+  enabled BOOLEAN
+) {
+  FOR $row IN ethereum_bridge.info() {
+    RETURN $row.chain, $row.escrow, $row.epoch_period, $row.erc20, $row.decimals, $row.balance, $row.synced, $row.synced_at, $row.enabled;
+  }
+};
+
 CREATE OR REPLACE ACTION ethereum_wallet_balance($wallet_address TEXT) PUBLIC VIEW RETURNS (balance NUMERIC(78, 0)) {
-  $balance := mainnet_bridge.balance($wallet_address);
+  $balance := ethereum_bridge.balance($wallet_address);
   RETURN $balance;
 };
 
 CREATE OR REPLACE ACTION ethereum_bridge_tokens($recipient TEXT DEFAULT NULL, $amount TEXT) PUBLIC {
-  mainnet_bridge.bridge(COALESCE($recipient, @caller), $amount::NUMERIC(78, 0));
+  ethereum_bridge.bridge(COALESCE($recipient, @caller), $amount::NUMERIC(78, 0));
 };
