b7945f28bb1bbd2161c95ea6959f14e66456f6b9
williamrusdyputra <williamrusdyputra@gmail.com>
2025-11-03 12:44:28 +0700

    chore: add internal transfer fee

--- internal/migrations/erc20-bridge/002-public-transfer-actions.sql
+++ internal/migrations/erc20-bridge/002-public-transfer-actions.sql
@@ -13,6 +13,16 @@ CREATE OR REPLACE ACTION sepolia_transfer($to_address TEXT, $amount TEXT) PUBLIC
     ERROR('Transfer amount must be positive');
   }
 
+  $fee := 1000000000000000000::NUMERIC(78, 0); -- 1 TRUF with 18 decimals
+  $caller_balance := ethereum_bridge.balance(@caller);
+
+  IF ($caller_balance - $amount::NUMERIC(78, 0)) < $fee {
+    ERROR('Insufficient balance for transfer. Requires an extra 1 TRUF fee on top of the transfer amount');
+  }
+
+  $leader_addr TEXT := encode(@leader_sender, 'hex')::TEXT;
+  sepolia_bridge.transfer($leader_addr, $total_fee);
+
   -- Execute transfer using the bridge extension
   sepolia_bridge.transfer($to_address, $amount::NUMERIC(78, 0));
 };
