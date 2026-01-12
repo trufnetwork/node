cf5da930f37685bfdf254c6053d58be486448444
Michael Buntarman <michaelboentarman@gmail.com>
2026-01-12 23:52:48 +0700

    chore: change distribution period for eth and sepolia

--- internal/migrations/erc20-bridge/000-extension.sql
+++ internal/migrations/erc20-bridge/000-extension.sql
@@ -2,20 +2,21 @@
 -- USE erc20 {
 --     chain: 'sepolia',
 --     escrow: '0xf6009345d772a04f4e52150b5ec91617db79be58',
---     distribution_period: '30m'
+--     distribution_period: '10m'
 -- } AS sepolia_bridge;
 
 -- USE erc20 {
 --     chain: 'ethereum',
 --     escrow: '0x964dd66477c22BC5726dfeBCf9BA49aa8625978C',
---     distribution_period: '30m'
+--     distribution_period: '10m'
 -- } AS ethereum_bridge;
 
 -- TESTING ONLY ---
 -- The following is for test environments where the sepolia bridge is used. Please comment for production and use the above.
 USE erc20 {
     chain: 'sepolia',
-    escrow: '0x502430eD0BbE0f230215870c9C2853e126eE5Ae3'
+    escrow: '0x502430eD0BbE0f230215870c9C2853e126eE5Ae3',
+    distribution_period: '10m'
 } AS sepolia_bridge;
 
 -- The following is for test environments where the hoodi bridge is used. Please comment for production and use the above.
